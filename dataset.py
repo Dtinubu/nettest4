cb2ed1167becafaab2219d3cec01a2d2ae2f1fdb
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-01-13 12:07:05 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,8 +8,8 @@ import numpy as np
 
 from utils import image_loader, download
 
-DATASET_TARBALL = Users\ladet\Downloads\dataset\African\m.0_1dtzr
-PAIRS_TRAIN = Users\ladet\Downloads\dataset\Asian\m.0_1f7mv
+DATASET_TARBALL = \Users\ladet\Downloads\dataset\African
+PAIRS_TRAIN = \Users\ladet\Downloads\dataset\Asian
 PAIRS_VAL = "http://vis-www.cs.umass.edu/lfw/pairsDevTest.txt"
 
 def create_datasets(dataroot, train_val_split=0.9):
